e50ec9c9a97b142e65e1f07c256116c1f6fa9a3e
polllick <davidpollock15@gmail.com>
2021-01-04 20:12:37 -0400

    archives all urls in test.txt

--- urlarchiver.sh
+++ urlarchiver.sh
@@ -1,8 +1,16 @@
-touch archivelist.txt
+timestamp=$(date +"%F%T")
+
+output="archive-$timestamp.txt"
+touch $output
+
+echo "" > $output
 
 ia-saveurl() {
     curl -s -I "https://web.archive.org/save/$1" |
-    grep location >> archivelist.txt
+    grep location >> $output
+    #TODO curl –data url=$1 -X POST http://archive.is/submit/
 }
 
-ia-saveurl $1
+while IFS= read -r line; do
+    ia-saveurl $line
+done < $1
